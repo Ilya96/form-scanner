8dc81561a262563672fb2a6f0627667009d0ea16
Sinitca Aleksandr <siniza.s.94@gmail.com>
2020-01-31 21:47:10 +0300

    Fix pdf

--- form_scanner/form_saver.py
+++ form_scanner/form_saver.py
@@ -4,7 +4,7 @@ def save_form_batch(im_files_list:list, target_name):
     pdf = FPDF()
     for im_file in im_files_list:
         pdf.add_page()
-        pdf.image(im_file, x = 0, y = 0, w = 100)
+        pdf.image(im_file, x = 0, y = 0, w = 210)
 
     pdf.output(target_name, 'F')
     return
